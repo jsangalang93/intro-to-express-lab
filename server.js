51a7b90488880a572e0c019c6f072ee5cf50e47a
jsangalang93 <jsangalang93@gmail.com>
2024-04-20 17:20:49 -0400

    exercise 3 wip

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
 
+// EXERCISE 1
+
 app.get('/greeting', (req, res)=>{
     res.send(`Hello there ${req.query.name}! What a delight is to see you once more!`);
 })
+
+// EXERCISE 2
 
 app.get('/roll/:number', (req, res) => {
     const {number} = req.params
@@ -23,6 +27,8 @@ app.get('/roll/:number', (req, res) => {
     }
 });
 
+// EXERCISE 3
+
 app.get('/collectibles/:index', (req, res) => {
     const collectibles = [
         { name: 'shiny ball', price: 5.95 },
@@ -30,15 +36,18 @@ app.get('/collectibles/:index', (req, res) => {
         { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
       ];
     
-    const {index} = req.params
+    const index = req.query.index
+    const name = req.query.name
+    const price = req.query.price
 
-        if( isNaN (index)) {
+        if( ((index) <2) || ((index) > 0 ) || (isNaN (index)) ) {
+
             res.send(`This item is not yet back in stock. Check back soon!`)
         } else {
-            const item = req.query.collectibles.name
-            const price = req.query.collectibles.price
-
-            res.send(`So you want a ${item}? For ${price}, it can be yours!`);
+            const index = req.query.index
+            const name = req.query.name
+            const price = req.query.price
+            res.send(`So you want a ${name}? For ${price}, it can be yours!`);
         }
 });
 
