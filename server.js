d4510c4e1d4a678ccfdba49df936666f40baed39
jsangalang93 <jsangalang93@gmail.com>
2024-04-21 14:14:10 -0400

    through exercise 3

--- server.js
+++ server.js
@@ -36,20 +36,31 @@ app.get('/collectibles/:index', (req, res) => {
         { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
       ];
     
-    const index = req.query.index
-    const name = req.query.name
-    const price = req.query.price
+    // const index = parseInt(req.params.index)
+    // const name = req.params.find(collectibles.index.name)
 
-        if( ((index) <2) || ((index) > 0 ) || (isNaN (index)) ) {
+    const index = parseInt(req.params.index)
+    const name = collectibles [index].name
+    const price = collectibles [index].price
+    // const price = req.params.index.price
+    //   const name = req.params.name
+    //   const price = req.params.price
 
-            res.send(`This item is not yet back in stock. Check back soon!`)
-        } else {
-            const index = req.query.index
-            const name = req.query.name
-            const price = req.query.price
-            res.send(`So you want a ${name}? For ${price}, it can be yours!`);
-        }
+            if (index >= 0 && index <= collectibles.length) {
+
+                // console.log('yes');
+                // console.log(index);
+               
+                res.send(`So you want a ${name}? For $${price}, it can be yours!`);
+            } else {
+                res.send(`This item is not yet back in stock. Check back soon!`)
+                // console.log ('no')
+            }
+
 });
+
+
+
 
 // app.get('/collectibles/:index', (req, res) => {
 //     const {index} = req.params
@@ -61,6 +72,35 @@ app.get('/collectibles/:index', (req, res) => {
 //         }
 // });
 
+// exercise 4
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+const type = shoes[index].type
+const indexShoes = parseInt(req.params.index)
+
+
+app.get('/shoes/', (req, res) => {
+    console.log('this works');
+})
+
+//     for (let indexShoes = 0; index <= indexShoes.length; indexShoes++) {
+//         if(type = 'sandal'){
+//         console.log('this works');
+//         }
+//     }
+// });
+
 app.listen(3000, () =>{
     console.log('listening on port 3000')
     });
+    
+    // res.send(shoes)
